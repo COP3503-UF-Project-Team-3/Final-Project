b88198c008afabd82ee0e0b130a4ab0f017482c3
jazy5552 <jazy555@hotmail.com>
2016-11-28 14:35:19 -0500

    Complete implementation of the store menu

--- src/store.h
+++ src/store.h
@@ -1,6 +1,7 @@
 #ifndef STORE_H
 #define STORE_H
 #include "menu.h"
+#include "item.h"
 
 /*
  *	The store menu
@@ -13,20 +14,24 @@ class StoreMenu:Menu {
 public:
 	//My play function for the store
 	void play(Player &p);
+	//Change the prices of the items randomly d amount of times
+	static void simulateMarket(std::vector<Item> &v);
 
 private:
-	static int lastDate; //The last day the player entered the store
+	static int LAST_DATE; //The last day the player entered the store
 
 	//Prints the menu (Won't actually, I need the player object)
 	void printMenu();
-	//Prints the menu with the item prices
-	void printMenu(const Player &p);
+	//Prints the menu with the item prices (Mode is buy/sell)
+	void printMenu(const Player &p, bool mode);
 
-	//Change the prices of the items randomly d amount of times
-	void modifyPrices(std::vector<Item> &v);
 	//Will create the items and store them into the player
 	void createItems(Player &p);
 
-}
+	//Purchase the item at the given index
+	void purchase(Player &p, int item);
+	//Sell the item at the given index
+	void sell(Player &p, int item);
+};
 
 #endif
