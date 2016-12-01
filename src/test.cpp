d5d4c3a6fcb516efd83e39776457b21a9d4f8891
jazy5552 <jazy555@hotmail.com>
2016-12-01 18:37:16 -0500

    Add more comments to test and simple instructions at the top

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,24 @@
+/*
+ *	This file is intended for menu testing. Simply include your Menu class at the top and
+ *	create an instance of it in the main function below. You can also look at/modify the
+ *	sample MyMenu class I made below and instantiate that.
+ *
+ *	To build the project simply run `make` in cmd/terminal
+ *	A finalproject.out will be created which you can run to test the game
+ *
+ *	When ever you want to rebuild completely it is best to first run `make clean` then run `make`
+ *	This ensures that any stray object files are rebuilt. Useful for when switch branches/changed header files/or just clearing
+ *	up the workspace.
+ */
+
 #include <iostream>
 #include "menu.h"
-#include "title.h"
+#include "school.h" //PLACE YOUR HEADER FILE HERE
 
 using namespace std;
 
 //My own menu declaration, inheriting from the Menu class
 class MyMenu:Menu {
-
 	//My print menu (Can be omitted to do nothing)
 	void printMenu() {
 		cout << "Hello World \n" <<
@@ -14,7 +26,6 @@ class MyMenu:Menu {
 		"Every key you hit will cost you an hour\n\n" <<
 		"Hit q to quit\n";
 	}
-
 	//NOTE: Play must be public
 public:
 	//The play function that contains MyMenu's game logic (loop)
@@ -41,15 +52,19 @@ public:
 		//Simply exit out of the function and return to whomever
 		//called this play function
 	}
-
 };
 
+/*
+ *	The main function. Instantiate your menu class here and run play on it to test it
+ *	A sample player object is created for testing purposes here as well
+ */
+
 int main() {
-	TitleMenu m;
+	School m; //Replace this variable type with the name of your class to test it
 	//Create player for testing
 	Player p(false);
 	p.name = "Bobby";
-	p.intelligence = 10;
+	p.intelligence = 5;
 	p.strength = 5;
 	p.dollars = 200;
 	p.hours = 5;
@@ -57,10 +72,10 @@ int main() {
 
 	// For testing
 	// p.save();
-
-	//Call my play function
-	m.play(p);
 	
+	cout << "Initializing game..." << endl;
+	//Call play function
+	m.play(p);
 	cout << endl << "Done playing" << endl;
 	return 0;
 }
