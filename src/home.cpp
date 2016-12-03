cdc8b0543aba68ff0935170fe4786c5321e7daeb
Adam Haubenstock <aehaubenstock@gmail.com>
2016-12-03 18:31:57 -0500

    Fix choice loop

--- src/home.cpp
+++ src/home.cpp
@@ -10,41 +10,44 @@ void Home::play(Player &p) {
 	printStatus(p);
 
 	// Choice loop
-	char c;
-	bool once = false;
-	do {
-		if (once) {
-			string msg = "`" + string(1,c) + "` " + " is an invalid choice.";
-			updateStatus(p, msg);
-		}
-		once = true;
-		c = getKey();
-	} while (c != '1' && c != '2' && c != '3');
 
-	if (c == '1') {
-		p.hours = 18;
-		++p.day;
-		string msg = "Hours replensished, ";
-		if (p.save()) {
-			msg += "game saved. ";
+	char c;
+    do {
+        c = getKey();
+
+	    if (c == '1') {
+			p.hours = 18;
+			++p.day;
+			string msg = "Hours replensished, ";
+			if (p.save()) {
+				msg += "game saved. ";
+			} else {
+				msg += "unable to save game. ";
+			}
+			msg += " Make another choice.";
+			updateStatus(p, msg);
+		} else if (c == '2') {
+			// Do nothing
+		} else if (c == '3') {
+			// Do nothing
 		} else {
-			msg += "unable to save game. ";
-		}
-		msg += " Hit any key to continue...";
-		updateStatus(p, msg);
-		getKey();
-		play(p);
-	} else if (c == '2') {
-		// Leave house, so do nothing
-	} else if (c == '3') {
-		main(); // Essentially restart the program
+            // Choice is not '1'-'3'
+            string err = "`" + string(1, c) + "` is not a valid choice.";
+            updateStatus(p, err);
+        }
+
+    } while (c != '2' && c != '3');
+
+	if (c == '3') {
+		main(); // Essentially restart the game
 	}
+	
 }
 
 void Home::printMenu() {
 	cout << "Home" << endl;
 	cout << "\t" << "1. Sleep (save & replenish hours)" << endl;
 	cout << "\t" << "2. Leave house" << endl;
-	cout << "\t" << "3. Exit (without saving)" << endl;
+	cout << "\t" << "3. Exit to title screen (without saving)" << endl;
 	cout << "Make a choice." << endl;
 }
