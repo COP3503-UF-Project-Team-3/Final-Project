fa3f88d74d1d1978c89d4f335f86919a29b648d5
jazy5552 <jazy555@hotmail.com>
2016-12-03 19:28:44 -0500

    Correct stats formatting

--- src/stats.cpp
+++ src/stats.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 
 void StatsMenu::play(Player &p) {
+	clearConsole();
 	cout << endl;
 	cout << "Stats" << endl;
 	cout << endl;
 	cout << "Name: " << p.name << endl;
 	cout << "Intelligence: " << p.intelligence << endl;
-  	cout << "Strength: " << p.strength << endl;
+	cout << "Strength: " << p.strength << endl;
 	cout << endl;
 	cout << "Press any key to continue." << endl;
 	getKey();
